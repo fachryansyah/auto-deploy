632994f1e641fa02668ed1121098ceea4b8745db
fahriansyah <fachryansyah123@gmail.com>
2020-11-30 00:57:11 +0700

    update: load config from .env

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 3000
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 6969;
 
 // webhook
 app.post('/', (req, res) => {
   const projectName = req.project.name;
-  shellExec(`./deploy_${projectName}`).then(console.log).catch(console.log)
+  shellExec(`./deploy_${projectName}`).then(console.log).catch(console.log);
 })
 
 app.listen(port, () => {
